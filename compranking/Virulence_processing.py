ed237a64ca5335e194089803dbdc246daf85dd19
gaoyangluo <lgyjsnjhit@gmail.com>
2022-10-26 17:24:24 +0800

    rename virulence summary file

--- compranking/Virulence_processing.py
+++ compranking/Virulence_processing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# title             :Virulence_processing.py
+# title             :Virulence_processing.py -> VFDB&PATH_processing.ipynb
 # description       :process all the outputs
 # author            :Gaoyang Luo
 # date              :202201019
@@ -36,8 +36,29 @@ def VF_processing(input_contig, input_ERR_VFDB_output,input_cpr_VF_sum,output,fi
     df_VFDB_output_annote=df_VFDB_output_annote[[0,"id","sub_id","VFID","Virulence_factor","VF_Name","Bacteria","VFcategory","ESKAPE"]]
     df_VFDB_output_annote.columns=["Contigs","id","sub_id","VFID","Virulence_factor","VF_Name","Bacteria","VFcategory","ESKAPE"]
     
-    #save
+    #PATH-like sequences processing
+    #load patric output
+    df_patric=pd.read_csv(input_patric, sep="\t", header=None)
+    df_patric.columns=['id', 'sub_id', 'identity', 'alignLen', 'mismat', 'gapOpens', 'qStart', 'qEnd', 'sStart', 'sEnd', 'eval', 'bit']
+    #iden60 and alen150
+    df_patric_iden60=df_patric[df_patric.identity > 80]
+    df_patric_den60_alen150=df_patric_iden60[df_patric_iden60.alignLen > 150]
+    df_PATH_contig=df_patric_den60_alen150[["id"]].drop_duplicates()
+    df_PATH_contig["Pathogenicity"]="Pathogenic"
+    #first merge VFs with index
+    df_VFs_contig=pd.merge(df_contig,df_VFDB_output_annote,left_on=1,right_on="id",how="left")
+    df_VFs_PATH_contig=pd.merge(df_VFs_contig,df_PATH_contig,left_on=0,right_on="id",how="left")
+    #drop
+    df_VFs_PATH_contig=df_VFs_PATH_contig.drop(["Contigs","id_x","sub_id","VFID","id_y"],axis=1,inplace=False)
+    #fill pathogenicity
+    df_VFs_PATH_contig=df_VFs_PATH_contig.fillna("-")
+    for index ,name in df_VFs_PATH_contig.iterrows():
+        if df_VFs_PATH_contig["Virulence_factor"][index] != "-":
+            df_VFs_PATH_contig["Pathogenicity"][index]="Pathogenic"
+    #rename column
+    df_VFs_PATH_contig.columns=["Contig","ORF","Virulence_factor","VF_Name","VF_Host_Bacteria","VFcategory","ESKAPE","Pathogenicity"]
     
+    #save
     VFfile=df_VFDB_output_annote.to_csv(output + "/CompRanking_"+filebase+"_Virulence_VFDB_output.tsv",sep="\t",index=None)
     
     return VFfile
@@ -54,13 +75,18 @@ if __name__ == "__main__":
     
     input_contig="/lomi_home/gaoyang/software/CompRanking/test/CompRanking/CompRanking_intermediate/preprocessing/5M_contigs/ERR1191817.contigs_5M_contigs.index"
     input_ERR_VFDB_output="/lomi_home/gaoyang/software/CompRanking/test/CompRanking/CompRanking_intermediate/Virulence/ERR1191817.contigs_5M_contigs_VFDB_setA1e-5.out"
-    input_cpr_VF_sum="/lomi_home/gaoyang/software/CompRanking/databases/CompRanking_VirulenceDB/CompRanking_HMM_Virulence_Summary.csv" 
- 
+    
+    input_patric="/lomi_home/gaoyang/software/CompRanking/test/CompRanking/CompRanking_intermediate/Virulence/PATRIC/ERR1191817.contigs_5M_contigs_PATRIC.out"
+    
     #gloab settings
-    input_dir="/lomi_home/gaoyang/software/CompRanking/test"
+    #flexible setting
+    input_dir="/lomi_home/gaoyang/software/CompRanking/test" 
+    #fixed setting
+    input_cpr_VF_sum="../databases/CompRanking_VirulenceDB/CompRanking_Virulence_Summary.csv" 
     output=os.path.join(input_dir,"CompRanking/CompRanking_result")
     file_abs_path=path.file_abs_path_list_generation(input_dir)
     file_name_base = path.file_base_acquire(file_abs_path)
+    
     
     # print(file_abs_path)
     for i in file_name_base:
