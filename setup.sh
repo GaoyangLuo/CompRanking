efc568cffbdf49fa66f93c1cc02a533fe200c3e3
gaoyangluo <lgyjsnjhit@gmail.com>
2022-11-21 21:25:41 +0800

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -5,17 +5,20 @@ SCRIPT=$(readlink -f $0)
 SCRIPT_PATH=$(dirname ${SCRIPT})
 
 #config conda evn
-conda env create -f preprocessing.yaml
-conda env create -f seeker.yaml
-conda env create -f dvf.yaml
-conda env create -f PlasFlow.yaml
-conda env create -f rgi.yaml
+conda env create -f envs/alignment.yaml
+conda env create -f envs/deeparg.yaml
+conda env create -f envs/preprocessing.yaml
+conda env create -f envs/seeker.yaml
+conda env create -f envs/dvf.yaml
+conda env create -f envs/PlasFlow.yaml
+conda env create -f envs/rgi.yaml
+conda env create -f envs/hmmer.yaml
 
 #adjust seeker env
 CONDA_BIN=`cat ${SCRIPT_PATH}/test_yaml.yaml |shyaml get-value CompRanking.abs_path_to_conda_bin`
-CONDA_PATH=$(basename ${CONDA_BIN})
+CONDA_PATH=$(dirname ${CONDA_BIN})
 SEEKER_DIR=${CONDA_PATH}/envs/seeker/lib/python3.7/site-packages/seeker
-rm ${SEEKER_DIR}/command_line.py && cp scripts/command_line.py ${SEEKER_DIR}
+rm ${SEEKER_DIR}/command_line.py && cp ${SCRIPT_PATH}/scripts/command_line.py ${SEEKER_DIR}
 
 #check 
 
