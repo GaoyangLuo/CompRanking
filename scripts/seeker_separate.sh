7bec4b2131acd7130819ac7dd7eceeea19cf0593
gaoyangluo <lgyjsnjhit@gmail.com>
2023-03-05 18:51:55 +0800

    add threads control

--- scripts/seeker_separate.sh
+++ scripts/seeker_separate.sh
@@ -18,6 +18,23 @@ while getopts "p:i:m:t:o" option; do
 done
 
 
+# Step1 创建有名管道
+[ -e ./fd1 ] || mkfifo ./fd1
+
+# 创建文件描述符，以可读（<）可写（>）的方式关联管道文件，这时候文件描述符3就有了有名管道文件的所有特性
+exec 3<> ./fd1   
+
+# 关联后的文件描述符拥有管道文件的所有特性,所以这时候管道文件可以删除，我们留下文件描述符来用就可以了
+rm -rf ./fd1                    
+
+# Step2 创建令牌 
+for i in `seq 1 12`;
+do
+    # echo 每次输出一个换行符,也就是一个令牌
+    echo >&3                   
+done
+
+
 #run Seeker
 source ${CONDA_BIN_PATH}/activate CompRanking_seeker_env
 if [ -e checkdone/${PREFIX}.SEEKER.done ]; then
@@ -29,14 +46,21 @@ else
 	#Running Seeker
 	for i in ${INPUT_DIR}/${PREFIX}/CompRanking_intermediate/preprocessing/5M_contigs/*.fa
 	do
+	
+	read -u3                    # read 命令每次读取一行，也就是拿到一个令牌 
 	{
 		sleep 5
 		echo "${i} 正在执行phage预测"
 		predict-metagenome ${i}
+		echo >&3                # 执行完一条命令会将令牌放回管道
 		
 	}&
 	done
 	wait
+
+	exec 3<&-                       # 关闭文件描述符的读
+	exec 3>&-                       # 关闭文件描述符的写
+
 	#finish Running Seeker
 	echo "[TIMESTAMP] $(date) Running the second round phage prediction... Done"
 	ENDTIME=$(date +%s)
